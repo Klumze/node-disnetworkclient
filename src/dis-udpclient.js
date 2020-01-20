50a10254a522051bc5640170bc4698b959dc20ea
Michael Poulin <michael.poulin@msec.local>
2020-01-20 08:42:05 -0500

    Updated to local copy of newest open-dis library and added some PDU

--- src/dis-udpclient.js
+++ src/dis-udpclient.js
@@ -6,7 +6,7 @@ const DISUtils = require('./DISUtils');
 var utils = new DISUtils();
 
 var DEFAULT_HOST = '127.0.0.1';
-const DEFAULT_PORT = 3000
+const DEFAULT_PORT = 9000;
 
 /**
  * Read host & port as commandline arguments - or take the default if not given:
@@ -20,29 +20,79 @@ var client = dgram.createSocket('udp4');        /** Open a UDP-Client */
 /**
  * Create a Dummy EntityState-PDU and fill it with dummy data - just for testing:
  */
-var disEntityStatePDU= new dis.EntityStatePdu()
+var disEntityStatePDU= new dis.EntityStatePdu();
 disEntityStatePDU.entityID.site = 11;
 disEntityStatePDU.entityID.application = 22;
 disEntityStatePDU.entityID.entity = 33;
-disEntityStatePDU.marking.setMarking("Example Entity")
+disEntityStatePDU.marking.setMarking("Example Entity");
+
+var disTransmitterPDU= new dis.TransmitterPdu();
+disTransmitterPDU.entityId.site = 12;
+disTransmitterPDU.entityId.application = 23;
+disTransmitterPDU.entityId.entity = 34;
+disTransmitterPDU.radioId = 10;
+disTransmitterPDU.frequency = '100000';
+
+var disSignalPDU = new dis.SignalPdu();
+disSignalPDU.entityId.site = 13;
+disSignalPDU.entityId.application = 24;
+disSignalPDU.entityId.entity = 34;
+disSignalPDU.radioId = 11;
+
+var disReceiverPDU = new dis.ReceiverPdu();
+disReceiverPDU.entityId.site = 14;
+disReceiverPDU.entityId.application = 25;
+disReceiverPDU.entityId.entity = 35;
+disReceiverPDU.radioId = 12;
 
 
-var entityID = disEntityStatePDU.entityID;
-var location = disEntityStatePDU.entityLocation;
-var marking  = disEntityStatePDU.marking.getMarking();
-console.log("Sending EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'" )
+var ePDUentityID = disEntityStatePDU.entityID;
+var ePDUlocation = disEntityStatePDU.entityLocation;
+var ePDUmarking  = disEntityStatePDU.marking.getMarking();
+console.log("Sending EntityState:", ePDUentityID, "Location", ePDUlocation, "Marking: \'" + ePDUmarking + "\'" );
+
+var tPDUentityID = disTransmitterPDU.entityId;
+var tPDUradioID = disTransmitterPDU.radioId;
+var tPDUfrequency = disTransmitterPDU.frequency;
+console.log("Sending Transmitter:", tPDUentityID, "Radio", tPDUradioID, "Frequency", tPDUfrequency);
+
+var sPDUentityID = disSignalPDU.entityId;
+var sPDUradioID = disSignalPDU.radioId;
+console.log("Sending Transmitter:", sPDUentityID, "Radio", sPDUradioID);
+
+var rPDUentityID = disReceiverPDU.entityId;
+var rPDUradioID = disReceiverPDU.radioId;
+console.log("Sending Transmitter:", rPDUentityID, "Radio", rPDUradioID);
 
 
 /**
  * Encode the PDU intoto networkbuffer:
  */
-message = utils.DISPduToBuffer(disEntityStatePDU);
+var entPdu = utils.DISPduToBuffer(disEntityStatePDU);
+var tranPdu = utils.DISPduToBuffer(disTransmitterPDU);
+var sigPdu = utils.DISPduToBuffer(disSignalPDU);
+var recPdu = utils.DISPduToBuffer(disReceiverPDU);
 
 /**
  * Send the message on network and finish
  */
-client.send(message, 0, message.length, port, host, function(err, bytes) {
+client.send(entPdu, 0, entPdu.length, port, host, function(err, bytes) {
     if (err) throw err;
     console.log('UDP message sent to ' + host +':'+ port);
-    client.close()
+});
+
+client.send(tranPdu, 0, tranPdu.length, port, host, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + host +':'+ port);
+});
+
+client.send(sigPdu, 0, sigPdu.length, port, host, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + host +':'+ port);
+});
+
+client.send(recPdu, 0, sigPdu.length, port, host, function(err, bytes) {
+    if (err) throw err;
+    console.log('UDP message sent to ' + host +':'+ port);
+    client.close();
 });
--- src/dis-udpserver.js
+++ src/dis-udpserver.js
@@ -1,24 +1,24 @@
 const dgram = require('dgram');
 const argv = require('yargs').argv;
-const dis = require("open-dis")
+const dis = require("open-dis");
 const DISUtils = require('./DISUtils');
 
 var utils = new DISUtils();
 
-
-const DEFAULT_PORT = 3000
+const DEFAULT_MULTICAST = '239.10.229.10';
+const DEFAULT_HOST = '127.0.0.1';
+const DEFAULT_PORT = 9000;
 
 /**
- * Read port as commandline arguments - or take the default if not given:
+ * Read host and port as commandline arguments - or take the default if not given:
  */
-var port = argv.port || DEFAULT_PORT
-
+var multicast = argv.multicast || DEFAULT_MULTICAST;
+var port = argv.port || DEFAULT_PORT;
+var host = argv.host || DEFAULT_HOST;
 
 var server = dgram.createSocket('udp4');
-
 /**
  * Messagehandler
- * 
  */
 server.on('message', (msg, rinfo) => {
   console.log(`server got msg from ${rinfo.address}:${rinfo.port}`);
@@ -28,44 +28,64 @@ server.on('message', (msg, rinfo) => {
     var disMessage = utils.DISObjectFromBuffer(msg);
     switch(disMessage.pduType)
     {
-        case 1: // EntityState PDU:
-            var entityID = disMessage.entityID;
-            var location = disMessage.entityLocation;
-            var marking  = disMessage.marking.getMarking();
+		case 1: // EntityState PDU:
+		    var entityID = disMessage.entityID;
+		    var location = disMessage.entityLocation;
+		    var marking  = disMessage.marking.getMarking();
 
-            console.log("Got EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'"             )
-           
-            break;
-        case 20: // Data PDU:
-            console.log("Got DataPDU:")
-            break;
-        default:
-            console.log("Got Other PDU:", )
+		    console.log("Got EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'");
+		   
+		    break;
+		case 20: // Data PDU:
+		    console.log("Got DataPDU:")
+		    break;
+		case 25:
+			var entityID = disMessage.entityId;
+			var radioID = disMessage.radioId;
+			var frequency = disMessage.frequency;
+			console.log("Got TransmitterPDU:", entityID, "Radio ID", radioID, "Frequency", frequency);
+			break;
+		case 26:
+			var entityID = disMessage.entityId;
+			var radioID = disMessage.radioId;
+			console.log("Got SignalPDU:", entityID, "Radio:", radioID);
+			break;
+		case 27:
+			var entityID = disMessage.entityId;
+			var radioID = disMessage.radioId;
+			console.log("Got ReceiverPDU:", entityID, "Radio:", radioID);
+			break;
+		default:
+		    console.log("Got Other PDU:", );
     }
   }catch(e)
   {
-      console.log("Exception:",disMessage.pduType)
+      console.log("Exception:", disMessage.pduType);
   }
- 
 });
 
 /**
  * Some debug message to show errors
  */
 server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-  });
+	console.log(`server error:\n${err.stack}`);
+	server.close();
+});
 
 /**
  * Some debug message to show, when the server started listening
  */
 server.on('listening', () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+	const address = server.address();
+	console.log(`server listening ${address.address}:${address.port}`);
 });
 
 /**
  * Actually start listening on port:
  */
-server.bind(port);
+server.bind(port, host, () => {
+	server.setBroadcast(true);
+	server.setMulticastTTL(5);
+	server.addMembership(multicast);
+	console.log(`Subscribed to multicast group ${multicast}`);
+});
