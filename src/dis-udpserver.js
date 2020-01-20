12da4f0c4f83c13774e4f4f1a199122a37269264
Michael Poulin <michael.poulin@force.gc.ca>
2020-01-19 23:19:24 -0500

    Revert "Initial set of asudio sim pdus added to client and server."
    This reverts commit 9e8e9941b94d11987e8461e558c0c7b302be4758.

--- src/dis-udpserver.js
+++ src/dis-udpserver.js
@@ -1,20 +1,17 @@
 const dgram = require('dgram');
 const argv = require('yargs').argv;
-const dis = require("open-dis");
+const dis = require("open-dis")
 const DISUtils = require('./DISUtils');
 
 var utils = new DISUtils();
 
-const DEFAULT_HOST = "127.0.0.1";
-const DEFAULT_MULTICAST = "239.10.229.10";
-const DEFAULT_PORT = 9000;
+
+const DEFAULT_PORT = 3000
 
 /**
  * Read port as commandline arguments - or take the default if not given:
  */
-var port = argv.port || DEFAULT_PORT;
-var host = argv.host || DEFAULT_HOST;
-var multicast = argv.multicast || DEFAULT_MULTICAST;
+var port = argv.port || DEFAULT_PORT
 
 
 var server = dgram.createSocket('udp4');
@@ -28,36 +25,26 @@ server.on('message', (msg, rinfo) => {
 
   try
   {
-	var disMessage = utils.DISObjectFromBuffer(msg);
+    var disMessage = utils.DISObjectFromBuffer(msg);
     switch(disMessage.pduType)
     {
         case 1: // EntityState PDU:
+            var entityID = disMessage.entityID;
             var location = disMessage.entityLocation;
             var marking  = disMessage.marking.getMarking();
-            var entityID = disMessage.entityID;
- 
+
             console.log("Got EntityState:", entityID, "Location", location, "Marking: \'" + marking + "\'"             )
            
             break;
         case 20: // Data PDU:
-            console.log("Got DataPDU:", entityID);
-			break;
-		case 25: // Transmitter PDU:
-			var transmitter = dis.TransmitterPdu();	
-            console.log("Got TransmitterPDU:", transmitter);
-			break;
-		case 26: // Singal PDU:
-			console.log("Got SingalPDU:", entityID);
-			break;
-		case 27: // Receiver PDU:
-			console.log("Got ReceiverPDU:", entityID);
-			break;
+            console.log("Got DataPDU:")
+            break;
         default:
             console.log("Got Other PDU:", )
     }
   }catch(e)
   {
-      console.log("Exception:", e)
+      console.log("Exception:",disMessage.pduType)
   }
  
 });
@@ -81,9 +68,4 @@ server.on('listening', () => {
 /**
  * Actually start listening on port:
  */
-server.bind(port, host, () => {
-	server.setBroadcast(true);
-	server.setMulticastTTL(5);
-	server.addMembership(multicast);
-	console.log(`Subscribing to muticast group ${multicast}`);
-});
+server.bind(port);
